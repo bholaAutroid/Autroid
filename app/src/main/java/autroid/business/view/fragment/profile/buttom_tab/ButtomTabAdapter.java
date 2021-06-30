83baf9b16ad1b26c5afb77eae687322451c05023
Bhola-kr <bholakumar2298@gmail.com>
2021-06-30 21:31:16 +0530

    Profile Tab,
    Nested profile and all Profile views updated bottom tab act work remaining

---
+++ app/src/main/java/autroid/business/view/fragment/profile/buttom_tab/ButtomTabAdapter.java
@@ -0,0 +1,41 @@
+package autroid.business.view.fragment.profile.buttom_tab;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import org.jetbrains.annotations.NotNull;
+
+public class ButtomTabAdapter extends FragmentPagerAdapter {
+    int tabcount;
+
+
+    public ButtomTabAdapter(@NonNull @NotNull FragmentManager fm, int behavior) {
+        super( fm, behavior );
+        tabcount = behavior;
+    }
+
+    @NonNull
+    @NotNull
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0:
+                return new ImageFragment1();
+            case 1:
+                return new OfferFragment2();
+            case 2:
+                return new ServiceFragment3();
+            case 3:
+                return new ReviewFragment4();
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return tabcount;
+    }
+}
